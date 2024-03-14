56a5c40b5587450776a0ee3e3284638a94b4157e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-14 21:16:20 +0300

    Create Week1-0011-Functions-Random.js

---
+++ Week1-0011-Functions-Random.js
@@ -0,0 +1,10 @@
+/*
+Your Goal: Complete getRandom function
+Inside getRandom, get a random number from the Math.random() function.
+Then, return that number!
+
+*/
+
+function getRandom() {
+    return Math.random();
+}
