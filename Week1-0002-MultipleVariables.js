2f7a60c633075550a0036d06e4c94bb1b0e9c550
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-14 21:00:51 +0300

    Create Week1-0002-MultipleVariables.js

---
+++ Week1-0002-MultipleVariables.js
@@ -0,0 +1,8 @@
+/*
+Your Goal: Create two equal variables
+Create two variables a and b with the same value in each. Similar to the example above, you can first declare a and then set b equal to a.
+
+*/
+
+const a = 21;
+const b = a;
