dfeef76dd75be52279713d53a06e55b42405ae78
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-14 21:07:14 +0300

    Create Week1-0006-Comments.js

---
+++ Week1-0006-Comments.js
@@ -0,0 +1,8 @@
+/*
+Your Goal: Fix the Comments!
+Somebody wrote some comments in the program, but forgot to use the correct syntax!
+
+*/
+
+//all shoes are on sale this week!
+const shoesCost = 49;
